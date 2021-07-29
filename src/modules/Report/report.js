e3c9561584afa05e0a3291164c694384492aaf6c
Roberto Freire Ferraz Passos <roberto.passos@radixeng.com.br>
2021-07-28 21:11:39 -0300

    Added sample of a chart and added the chartJs library

--- src/modules/Report/report.js
+++ src/modules/Report/report.js
@@ -1,7 +1,52 @@
+import { useEffect } from 'react';
+
+import Chart from './libraries/chart';
+
 function Report(){
+
+    useEffect(() => {
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    console.log("TEste");
+    }, [0]);
+
     return (        
         <div>
             <h2>Report</h2>
+            <canvas id="myChart" width="400" height="400"></canvas>
         </div>
     );
 }
