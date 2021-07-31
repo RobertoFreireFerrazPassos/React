0e5a03a19781db180dacb7fd8e4960dfcbb68a36
Roberto Freire Ferraz Passos <roberto.passos@radixeng.com.br>
2021-07-31 13:36:21 -0300

    changed navbar to component class based

--- src/core/navbar/navbar.js
+++ src/core/navbar/navbar.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Component } from 'react';
 
 import Option from './option/option';
 import DropdownOption from './dropdownOption/dropdownOption';
@@ -6,24 +6,35 @@ import { NavBarService } from './navBarService';
 
 import './navbar.css';
 
-function NavLeftBar(){
-    const [options, setOptions ] = useState(NavBarService.getNavBarOptions());
+class NavLeftBar extends Component {
+    constructor(){
+        super();
+        this.state = {
+            options : NavBarService.getNavBarOptions()
+        }
 
-    const subscritionNavBarService = NavBarService.onChanges().subscribe((navBarOptions)=>{
-        setOptions(navBarOptions);
-    });
+        const subscritionNavBarService = NavBarService.onChanges().subscribe((navBarOptions)=>{
+            this.setState({
+                options : navBarOptions
+            });
+        });
+    }
 
-    return <div className="navbar">            
-        <ul>
-            { options.map(option => {
-                if (option.dropdownOptions && option.dropdownOptions.length > 0){
-                    return <DropdownOption key={option.id} option={option}></DropdownOption>
-                }
-                    return  <Option key={option.id} active={option.active} option={option}></Option>;
-               })
-            }            
-        </ul>
-    </div>;
+    render(){
+        return (
+            <div className="navbar">            
+                <ul>
+                    { this.state.options.map(option => {
+                        if (option.dropdownOptions && option.dropdownOptions.length > 0){
+                            return <DropdownOption key={option.id} option={option}></DropdownOption>
+                        }
+                            return  <Option key={option.id} active={option.active} option={option}></Option>;
+                    })
+                    }            
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default NavLeftBar;
