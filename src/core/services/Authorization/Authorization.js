c371821adf3bd7e616598384a09762f9ada79686
Roberto Freire Ferraz Passos <roberto.passos@radixeng.com.br>
2021-08-02 18:52:16 -0300

    deleted authorization code not used

--- src/core/services/Authorization/Authorization.js
+++
@@ -1,5 +0,0 @@
-export const AuthorizationService = {
-    isLoged: () => localStorage.getItem("AuthorizationReactApp"),
-    login: () => localStorage.setItem("AuthorizationReactApp", true),
-    logout: () => localStorage.setItem("AuthorizationReactApp", false)
-};
